3b24770280e6d731b800d28cb3aefb5acda5afe8
Kris Buote <krisbuote@gmail.com>
2018-12-13 16:52:20 -0700

    working routes

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var usersRouter = require('./routes/users');
-var wikiRouter = require('./routes/wiki.js');
-
 var indexRouter = require('./routes/index.js');
 var stakesRouter = require('./routes/stakes.js');
 var aboutRouter = require('./routes/about.js');
 
-
 var app = express();
 
-// view engine setup
+// // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'pug');
 
@@ -25,26 +21,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// app.use('/wiki', wikiRouter);
-
 app.use('/stakes', stakesRouter);
 app.use('/about', aboutRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
